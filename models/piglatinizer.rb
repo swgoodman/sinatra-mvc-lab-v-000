1c547b2c38339be88d4a57bc4fa1d61ef27c7c42
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 01:25:45 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,7 +12,20 @@ class PigLatinizer
     vowels = %w[a e i o u]
     consonants = alpha - vowels
     @answer = []
-    binding.pry
 
+    @text.each do |word|
+      if vowels.include?(word[0].downcase)
+        @answer << word + 'way'
+      elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+        @answer << word[3..-1] + word[0..1] + word[1..2] + 'ay'
+      elsif consonants.include?(word[0]) && consonants.include?(word[1])
+        @answer << word[2..-1] + word[0..1] + 'ay'
+      elsif consonants.include?(word[0])
+        @answer << word[1..-1] + word[0] + 'ay'
+      else
+        @answer << word
+      end
+    end
+    @answer.join(',')
   end
 end
