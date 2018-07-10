65cd73fd882ec8da03e0a524e3cb8e2783d3cbc7
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 01:32:29 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -16,16 +16,16 @@ class PigLatinizer
     @text.each do |word|
       if vowels.include?(word[0].downcase)
         @answer << word + 'way'
-      elsif consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1]) && consonants.include?(word[2])
         @answer << word[3..-1] + word[0..1] + word[2] + 'ay'
-      elsif consonants.include?(word[0]) && consonants.include?(word[1])
+      elsif consonants.include?(word[0].downcase) && consonants.include?(word[1])
         @answer << word[2..-1] + word[0..1] + 'ay'
-      elsif consonants.include?(word[0])
+      elsif consonants.include?(word[0].downcase)
         @answer << word[1..-1] + word[0] + 'ay'
       else
         @answer << word
       end
     end
-    @answer.join(',')
+    @answer.join(',').gsub(/[\s,]/ ," ")
   end
 end
